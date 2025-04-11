aa8979a58fb13a77277c083de02f196c5ea67962
Vitor Gomes <vitor.araujo.derreteman4@gmail.com>
2025-04-10 23:45:54 -0300

    Adicionando exemplo de getter e setter na classe pessoa.py em poo-temp

--- poo-temp/classes/pessoa.py
+++ poo-temp/classes/pessoa.py
@@ -1,10 +1,21 @@
 class Pessoa():
-    def __init__(self, nome, idade, altura, peso, sexo):
-        self.nome = str(nome)
-        self.idade = int(idade)
-        self.altura = float(altura)
-        self.peso = float(peso)
-        self.sexo = str(sexo)
-    
+    def __init__(self, nome: str, idade: int, altura: float, peso: float, sexo: str):
+        self.nome = nome
+        self._idade = idade
+        self.altura = altura
+        self.peso = peso
+        self.sexo = sexo
+
     def __str__(self):
-        return "Nome: {}, Idade: {}, Altura: {}, Peso: {}, Sexo: {}".format(self.nome, self.idade, self.altura, self.peso, self.sexo)
+        return f"Nome: {self.nome}, Idade: {self.idade}, Altura: {self.altura}, Peso: {self.peso}, Sexo: {self.sexo}"
+
+    @property
+    def idade(self):
+        return self._idade
+    
+    @idade.setter
+    def idade(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Essa idade não é um numero inteiro")
+        else:
+            self._idade = value
--- poo-temp/main.py
+++ poo-temp/main.py
@@ -2,5 +2,3 @@ from agregadormodulo.agregador import Pessoa, Atendente, Farmaceutico
 pessoa = Pessoa("Nome", 20, 1.75, 55.5, "Masculino")
 print(pessoa)
 print()
-atendente = Atendente(pessoa.nome, pessoa.idade, pessoa.peso, pessoa.peso, pessoa.sexo, 543)
-print(atendente)
